62c6a5b905a5003d9481c79e34f764e8064ccbb6
Latte-JP <highpigment@gmail.com>
2025-09-21 00:25:04 +0900

    Update CameraManager.cs

--- Assets/Sprite/CameraManager.cs
+++ Assets/Sprite/CameraManager.cs
@@ -10,9 +10,11 @@ public class CameraManager : MonoBehaviour
     private float _shakeMagnitude;
     // private変数: スクリプト内で使用される
     private Player _player; // プレイヤーオブジェクトへの参照
+    // カメラの初期位置を保持する変数
+    private Vector3 initPos;
     private float _shakeCount;// 揺れ時間のカウント用
     private int _currentplayerHP;// 現在のプレイヤーのHPを格納
-                                 // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     [System.Obsolete]
     void Start()
@@ -21,12 +23,15 @@ public class CameraManager : MonoBehaviour
         _player = FindObjectOfType<Player>();
         // プレイヤーの現在のHPを初期値として格納
         _currentplayerHP = _player.GetHP();
+        // ゲーム開始時にカメラの現在位置を初期位置として記録
+        initPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         ShakeCheck(); // カメラ揺れチェックメソッドを呼び出す
+        _FollowPlayer();
     }
     // カメラ揺れチェックメソッド
     private void ShakeCheck()
@@ -40,11 +45,11 @@ public class CameraManager : MonoBehaviour
             _shakeCount = 0.0f;
             // カメラを揺らすコルーチンを開始
             StartCoroutine(_Shake());
-            
-            
+
+
         }
     }
-     // カメラを揺らすコルーチン
+    // カメラを揺らすコルーチン
     IEnumerator _Shake()
     {
         // カメラの初期位置を保存
@@ -69,5 +74,21 @@ public class CameraManager : MonoBehaviour
 
         // 揺れが終わったらカメラの位置を初期位置に戻す
         transform.position = initPos;
+    }
+        private void _FollowPlayer()
+    {
+        // プレイヤーのX座標を取得
+        float x = _player.transform.position.x;
+
+        // Mathf.ClampでカメラのX座標に制限を設ける
+        // 第1引数: 変更したい値 (プレイヤーのX座標)
+        // 第2引数: 最小値 (カメラの初期X座標)
+        // 第3引数: 最大値 (無限大 - 右方向には制限なし)
+        x = Mathf.Clamp(x, initPos.x, Mathf.Infinity);
+
+        // カメラの新しい位置を設定
+        // X座標はプレイヤーに追従し、制限を適用
+        // Y座標とZ座標は初期位置を保持
+        transform.position = new Vector3(x, initPos.y, initPos.z);
     }
 }
